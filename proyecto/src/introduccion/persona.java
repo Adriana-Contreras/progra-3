d4afb8005401d547a8264812e41c2e36dfe77676
Adriana Contreras <noodle78ctr@gmail.com>
2020-02-17 15:51:03 -0300

    manejo de while  2

--- proyecto/src/introduccion/persona.java
+++ proyecto/src/introduccion/persona.java
@@ -6,40 +6,26 @@ public class persona {
 
         Scanner leer =new Scanner(System.in);
   //leer cadenas
-  //      System.out.println("ingrese su nombre");
-//        String n1nombre = leer.nextLine();
-//        System.out.println("ingrese su apellido ");
-//        String n1apellido = leer.nextLine();
-//
-//        System.out.printf("su nombre completo es: %s %s ", n1nombre, n1apellido);System.out.println();
-//
+     System.out.println("ingrese su nombre");
+        String n1nombre = leer.nextLine();
+       System.out.println("ingrese su apellido ");
+        String n1apellido = leer.nextLine();
+        System.out.printf("su nombre completo es: %s %s ", n1nombre, n1apellido);System.out.println();
 
-//        System.out.println("ingrese su edad: ");
-//        int  miedad =  leer.nextInt();
-//        System.out.printf("su edada es: %d", miedad);
 
-//
-//        System.out.println("ingrese su edad");
-//        while (leer.hasNextInt())
-//        {
-//            int edad2 = leer.nextInt();
-//            System.out.println("su edad es: "+ edad2);
-//            System.out.println("ingreses su edad");
-//        }
-//       // -----sumar las primeras 5 edades----
+        System.out.println("ingrese su edad: ");
+        int  miedad =  leer.nextInt();
+       System.out.printf("su edada es: %d", miedad);
 
-//        System.out.println("ingrese la primera edad: ");
-//        int  edad1 =  leer.nextInt();
-//        System.out.println("ingrese la segunda edad: ");
-//        int  edad2 =  leer.nextInt();
-//        System.out.println("ingrese la tercera edad: ");
-//        int  edad3 =  leer.nextInt();
-//        System.out.println("ingrese la cuarta  edad: ");
-//        int  edad4 =  leer.nextInt();
-//        System.out.println("ingrese  la quinta  edad: ");
-//        int  edad5 =  leer.nextInt();
-//        int total = edad1 +  edad2 + edad3 + edad4 + edad5;
-//        System.out.printf("el total de edades  es: %d", total);
+
+        System.out.println("ingrese su edad");
+        while (leer.hasNextInt())
+      {
+          int edad2 = leer.nextInt();
+           System.out.println("su edad es: "+ edad2);
+            System.out.println("ingreses su edad");
+      }
+      // -----sumar las primeras 5 edades----
 
 //------------------------------------------------------------------------
         int total =0;
